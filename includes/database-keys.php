f90f3189f603816bcdfc4da76fde3c876f876cae
Dean James <dean.j.k.james@gmail.com>
2022-03-10 21:12:23 +0000

    Update database-keys.php

--- includes/database-keys.php
+++ includes/database-keys.php
@@ -1,3 +1,3 @@
 <?php
-$dbh = new PDO("mysql:host=localhost;dbname=" . getenv("DATABASE"), "root", "");
+$dbh = new PDO("mysql:host=localhost;dbname=" . getenv("DB_NAME"), getenv("DB_USER"), getenv("DB_PASS"));
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
